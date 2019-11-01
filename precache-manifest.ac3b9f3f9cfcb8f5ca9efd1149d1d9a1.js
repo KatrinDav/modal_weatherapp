e892c149fb1d68f0f41d081b357afd404ed3add2
Kasia Dav <k.dawidowicz2016@gmail.com>
2019-11-01 15:34:33 +0100

    Updates

---
+++ precache-manifest.ac3b9f3f9cfcb8f5ca9efd1149d1d9a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "483306b50cf949366bf3fee0323979e9",
+    "url": "/modal_weatherapp/index.html"
+  },
+  {
+    "revision": "dd427945d6e46f6a878b",
+    "url": "/modal_weatherapp/static/css/main.3b5eaaad.chunk.css"
+  },
+  {
+    "revision": "04b35abf28ff94b3116d",
+    "url": "/modal_weatherapp/static/js/2.b143e3e8.chunk.js"
+  },
+  {
+    "revision": "dd427945d6e46f6a878b",
+    "url": "/modal_weatherapp/static/js/main.66c6a59a.chunk.js"
+  },
+  {
+    "revision": "f16148856393062dc81a",
+    "url": "/modal_weatherapp/static/js/runtime-main.d68d33cf.js"
+  }
+]);
